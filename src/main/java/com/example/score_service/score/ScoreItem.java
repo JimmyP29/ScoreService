1e8df124ea1a9c01dbb982172d21a5c433f5aaef
James Privett <jamesprivett29@gmail.com>
2021-03-31 13:07:17 +0100

    Constructors, getters and setters

--- src/main/java/com/example/score_service/score/ScoreItem.java
+++ src/main/java/com/example/score_service/score/ScoreItem.java
@@ -4,4 +4,39 @@ public class ScoreItem {
     private Long id;
     private String name;
     private Integer score;
+
+    public ScoreItem(Long id, String name, Integer score) {
+        this.id = id;
+        this.name = name;
+        this.score = score;
+    }
+
+    public ScoreItem(String name, Integer score) {
+        this.name = name;
+        this.score = score;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getScore() {
+        return score;
+    }
+
+    public void setScore(Integer score) {
+        this.score = score;
+    }
 }
